fd119009fc2f35b7634dceff20eab8f2068bd91e
Paul Voccio <paul.voccio@rackspace.com>
2010-08-11 16:11:05 -0500

    auth, ha

--- rfidtunes/read_arduino.py
+++ rfidtunes/read_arduino.py
@@ -2,11 +2,16 @@ import serial
 import os
 ser = serial.Serial('/dev/tty.usbserial-A6008iaz')
 ser.baudrate = 2400
+
+auth = "code:0415ED3467"
+
 while 1:
   s = ser.read(15)
-#  if line != "":
-#      print line
-  print s
-  os.system("open -a itunes  http://streamer-ntc-aa01.somafm.com:80/stream/1021")
+  if s == auth:
+    print "authorized %s" % s
+    os.system("open -a itunes '/Users/pvoccio/Music/iTunes/iTunes Music/Mumford & Sons/Sigh No More/01 Sigh No More.mp3'") 
+  else:
+    print "rejected %s" % s
+  
+  ser.flushInput()
 
-ser.close()
